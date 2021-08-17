4f9153e132e13639caeaf1015818613ec24ea473
Akeel <akeelm_uk@hotmail.com>
2021-08-17 22:28:18 +0100

    implementing functionality for TodoItem

--- src/components/TodoItem/TodoItem.test.tsx
+++ src/components/TodoItem/TodoItem.test.tsx
@@ -1,35 +1,58 @@
-import { render } from '@testing-library/react'
+import { render, getByTestId, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import TodoItem from '.';
-import { ITodoItem } from './TodoItem'
+import { ITodoItemProps } from './TodoItem'
 
 describe('TodoItem tests', () => {
-    const mockTodoItem: ITodoItem = {
+    const mockCompleteHandler = (id: number, state: boolean) => jest.fn();
+    const mockDeleteHandler = (id: number) => jest.fn();
+    const mockEditFn = jest.fn()
+    const mockEditHandler = (id: number, string: string) => mockEditFn();
+    const mockTodoItem: ITodoItemProps = {
         id: 1,
         text: 'Need to write some tests',
-        completed: false
+        completed: false,
+        completeHandler: mockCompleteHandler,
+        deleteHandler: mockDeleteHandler,
+        editHandler: mockEditHandler
     };
 
    it('should be able to render', () => {
        const { container } = render(
-           <TodoItem
-               id={mockTodoItem.id}
-               text={mockTodoItem.text}
-               completed={mockTodoItem.completed}
-           />
+           <TodoItem {...mockTodoItem} />
        );
 
        expect(container).toMatchSnapshot();
    });
 
-   it ('should have an editable input', () => {
-       throw('not yet implemented');
+   it ('should have an editable input', async () => {
+       const { container } = render(<TodoItem {...mockTodoItem} />);
+       const text: Element = getByTestId(container, 'todo-item-text');
+       await userEvent.click(text);
+
+       const input: Element = getByTestId(container, 'todo-item-input');
+       await waitFor(() => expect(input).toBeTruthy());
+
+       await userEvent.type(input, 'test input');
+       expect(mockEditFn).toHaveBeenCalled();
    });
 
-   it ('should be able to mark as completed', () => {
-       throw('not yet implemented');
+   it ('should be able to mark as completed', async() => {
+       const { container } = render(
+           <TodoItem {...mockTodoItem} completed={true} />
+       );
+
+       const text: Element = getByTestId(container, 'todo-item-text');
+       expect(text).toHaveClass('todo-item__text--completed');
    });
 
    it('should be able to delete the todo', () => {
-       throw('not yet implemented');
+       const { container } = render(
+           <TodoItem {...mockTodoItem} />
+       );
+
+       const deleteButton: Element = getByTestId(container, 'delete-todo-button');
+       userEvent.click(deleteButton);
+       waitFor(() => expect(mockDeleteHandler).toHaveBeenCalled());
    });
 });
--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,19 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ITodo from '../../interfaces/ITodo';
 
-export interface ITodoItem {
-    id: number;
-    text: string;
-    completed: boolean;
+export interface ITodoItemProps extends ITodo {
+    completeHandler: (id: number, state: boolean) => void;
+    deleteHandler: (id: number) => void;
+    editHandler: (id: number, text: string) => void;
 }
 
-const TodoItem: React.FC<ITodoItem> = ({
+const TodoItem: React.FC<ITodoItemProps> = ({
     id,
     text,
-    completed
+    completed,
+    completeHandler,
+    deleteHandler,
+    editHandler
 }) => {
+    const [isEditing, setIsEditing] = useState(false);
+
+    const todoItemCheckboxHandler = () => {
+        completeHandler(id, !completed);
+    }
+
+    const deleteTodoHandler = () => {
+        deleteHandler(id);
+    }
+
+    const editTodoHandler = (event: React.SyntheticEvent<HTMLInputElement>) => {
+        editHandler(id, (event.target as HTMLInputElement).innerText)
+    }
+
     return (
         <div className="todo-item" data-testid="todo-item">
-
+            <input
+                type="checkbox"
+                data-testid="todo-item-checkbox"
+                title="Complete"
+                tabIndex={1}
+                onChange={todoItemCheckboxHandler}
+                checked={completed}
+            ></input>
+            {
+                isEditing ?
+                    <input
+                        type="text"
+                        data-testid="todo-item-input"
+                        title="Edit todo"
+                        onChange={editTodoHandler}
+                    ></input>
+                    :
+                    <span
+                        data-testid="todo-item-text"
+                        tabIndex={1}
+                        className={`todo-item__text ${completed ? 'todo-item__text--completed' : ''}`}
+                        onFocus={() => setIsEditing(true)}
+                    >
+                        {text}
+                    </span>
+            }
+            <button
+                title="Delete todo"
+                data-testid="delete-todo-button"
+                onClick={deleteTodoHandler}
+            >
+                Delete
+            </button>
         </div>
     );
 }
